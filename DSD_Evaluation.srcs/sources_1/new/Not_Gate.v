20d03af422fa1a0df2fd38b59d37fb52ae531836
Piyush-Sharma-0 <autri.piyush@ce.du.ac.in>
2025-11-19 23:27:01 +0530

    Delete DSD_Evaluation.srcs directory

--- DSD_Evaluation.srcs/sources_1/new/Not_Gate.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps //basic time unit of simulation- 1ns, precision- 1ps
-
-module Not_Gate(
-input a,
-output y
-    );
-assign y=~a;
-// |- or, ~- not, ^- xor, ~^- xnor,~|- nor, ~&- Nand
-endmodule
